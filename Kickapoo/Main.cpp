77b88a3e7f36b749f60d44e2852d88afad57425a
wach.piotrek@gmail.com <wach.piotrek@gmail.com@d16c65a5-d515-2969-3ec5-0ec16041161d>
2010-04-10 10:31:04 +0000

    Project check in

---
+++ Kickapoo/Main.cpp
@@ -0,0 +1,80 @@
+
+#include "Common.h"
+#include "Kickapoo.h"
+
+void onLostDevice();
+
+Font mousePosition;
+Game* g_Game;
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nShowCmd)
+{
+	g_Window()->initApplication( "IGK Framework", 1024, 768, hInstance, true );
+	g_Direct3D()->initDirect3D(32, true);
+	g_Renderer()->create();
+	g_Input()->init(hInstance, false);
+	g_Audio()->create();
+	g_Mouse()->create();
+
+	g_Game = new Game();
+	g_Game->create();
+
+	HRESULT hr;
+	g_Direct3D()->enableAlphaBlending(true);
+
+	while( !g_Window()->isDone() ) 
+	{			
+		g_Window()->updateApplication();
+
+		if(g_Window()->keyDown(VK_ESCAPE))
+			g_Window()->exitApplication();
+
+		g_Timer()->update();
+		g_Audio()->update();
+		g_Mouse()->update();
+		g_Input()->update();
+		g_World()->update(g_Timer()->getFrameTime());
+		g_Game->update();
+
+		getDevice()->Clear(0, NULL, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER,
+			D3DCOLOR_XRGB(0, 0, 0), 1.0f, 0);			
+		getDevice()->BeginScene();		
+
+		g_Renderer()->setIdentity();
+		g_Game->draw();
+		g_Mouse()->drawCursor();
+
+		getDevice()->EndScene();
+		hr = getDevice()->Present(NULL, NULL, NULL, NULL);
+
+		// obsluga utraty urzadzenia
+		if(hr == D3DERR_DEVICELOST) {
+			onLostDevice();
+		}
+	}
+
+	// zwolnienie nie zwolnionych zasobow
+	Texture::releaseResources();
+	Font::releaseResources();
+
+	g_Renderer()->release();
+	g_Audio()->release();
+	g_Direct3D()->releaseDirect3D();
+	g_Window()->shutdownApplication();
+	return 0;
+}
+
+void onLostDevice()
+{
+	Font::onLostDevice();
+	//g_Renderer()->onLostDevice();
+	g_Direct3D()->onLostDevice();
+
+	g_Direct3D()->restoreDevice();
+
+	g_Direct3D()->onResetDevice();
+	//g_Renderer()->onResetDevice();
+	Font::onResetDevice();
+
+	g_Direct3D()->setDefaultSettings();
+}
---
+++ Kickapoo/Window.cpp
@@ -0,0 +1,209 @@
+#include "common.h"
+#include "Window.h"
+
+// for physics editing
+std::vector<D3DXVECTOR2> points;
+std::vector<std::vector<D3DXVECTOR2>> lines;
+bool lmousebutton = true;
+bool editorMode = false;
+int mouseX=0, mouseY=0;
+bool pause;
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	return g_Window()->WndProc(hwnd, uMsg, wParam, lParam);
+}
+
+Window::Window()
+	: width(0), 
+	height(0), 
+	bits(0),
+	isInit_(false), 
+	isWindowed_(false), 
+	isDone_(false)
+{
+}
+
+Window::~Window()
+{
+}
+
+bool Window::initApplication(const string& appName, 
+	int width_, int height_, HINSTANCE hInstance_, bool isFullScreen)
+{
+	hInstance = hInstance_;
+	strAppName = appName;
+
+	width  = width_;
+	height = height_;
+
+	// rejestrowanie klasy okna
+	WNDCLASSEX wc = { sizeof(WNDCLASSEX), CS_HREDRAW | CS_VREDRAW,
+					::WndProc, 0L, 0L, 
+					hInstance, LoadIcon(NULL, "hearfire.ico"), 0, (HBRUSH)COLOR_ACTIVEBORDER, NULL,
+					strAppName.c_str(), NULL };
+
+	if(!RegisterClassEx( &wc )) {
+		return false;
+	}
+
+	// utworzenie okna
+	hWnd = CreateWindowEx(NULL, strAppName.c_str(), strAppName.c_str(), isFullScreen ? WS_POPUP | WS_SYSMENU | WS_VISIBLE: WS_OVERLAPPEDWINDOW | WS_VISIBLE | WS_SYSMENU
+						  | WS_CLIPCHILDREN | WS_CLIPSIBLINGS, 0, 0, width, height, NULL, NULL, hInstance, NULL);
+
+	if (hWnd == NULL)
+		return false;
+
+	ShowWindow(hWnd, SW_SHOW);
+	UpdateWindow(hWnd);
+
+	ShowCursor(false);
+
+	clearKeyState();
+	return true;
+}
+
+bool Window::shutdownApplication()
+{	
+	DestroyWindow(hWnd);
+	UnregisterClass(strAppName.c_str(), hInstance);
+	return true;
+}
+
+void Window::updateApplication()
+{
+	static MSG msg;
+	if(PeekMessage(&msg, hWnd, NULL, NULL, PM_REMOVE))
+	{
+		if(msg.message == WM_QUIT)
+			isDone_ = true;
+		else {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}	
+}
+
+LRESULT CALLBACK Window::WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_CLOSE:
+	case WM_DESTROY:
+	case WM_QUIT:
+		isDone_ = true;
+		PostQuitMessage(0);
+		return 0;
+
+	case WM_CHAR:
+		charPressed = (char)wParam;
+
+		if(charPressed == 'n' && editorMode)
+		{
+			if(points.size() >= 3)
+			{
+				bool success = g_World()->addShape(points);
+				if(!success) {
+					points.clear();
+					return 0;
+				}
+
+				lines.push_back(points);
+				
+				points.clear();
+				
+			}
+		}
+		else if(charPressed == 'p')
+		{
+			if(drawPoints)
+				drawPoints = false;
+			else drawPoints = true;
+		}
+		else if(charPressed == 'l')
+		{
+			if(drawLines)
+				drawLines = false;
+			else drawLines = true;
+		}
+		else if(charPressed == 'e')
+		{
+			if(editorMode)
+				editorMode = false;
+			else editorMode = true;
+		} else
+			if(charPressed == 'z') {
+				pause = !pause;
+			}else if(charPressed == 'u')
+				g_World()->swapGravity();
+		
+		break;
+
+	case WM_KEYUP:
+		updateKeyState((int)wParam, false);
+		break;
+
+	case WM_KEYDOWN:
+		updateKeyState((int)wParam, true);
+		break;
+
+ 	case WM_SETCURSOR:
+		SetCursor(LoadCursor(NULL, NULL));
+// 		SetClassLong(hwnd,    // window handle 
+// 			GCL_HCURSOR,      // ch9nge cursor 
+// 			(LONG)LoadCursor(hInstance, IDC_ARROW)2);   
+		//SetCursor(LoadCursor(NULL, IDC_CROSS));
+ 		break;
+
+	case WM_LBUTTONDOWN:
+		if(lmousebutton && editorMode)
+		{
+			points.push_back(D3DXVECTOR2(g_Mouse()->getX(), g_Mouse()->getY()));
+			lmousebutton = false;
+		}
+		
+		break;
+
+	case WM_LBUTTONUP:
+		lmousebutton = true;
+		break;
+
+	case WM_MOUSEMOVE:
+		mouseX = g_Mouse()->getX();
+		mouseY = g_Mouse()->getY();
+	break;
+
+
+	case WM_RBUTTONDOWN:
+		
+		if(editorMode)
+		{
+			if(points.size())
+			{
+				points.pop_back();
+			}
+			else
+			{
+				if(lines.size())
+				{
+					lines.pop_back();
+					g_World()->remove_last();
+
+					// 				std::vector<D3DXVECTOR2> points;
+					// 				points = lines[lines.size()-1];
+					// 				if(points.size())
+					// 				{
+					// 					lines.pop_back();
+					// 					points.pop_back();
+					// 					lines.push_back(points);
+					// 				}
+				}
+			}
+		}
+		
+		
+		break;
+	}
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+
+}
